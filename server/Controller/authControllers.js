11e742147cf952da3fcb95bdbec0e049187eb587
o5harshit <srivastavaharshit400@gmail.com>
2025-09-14 19:47:30 +0530

    Changed authcontroller.js

--- server/Controller/authControllers.js
+++ server/Controller/authControllers.js
@@ -1,7 +1,7 @@
-import UserModel from "../Models/UserModel.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import dotenv from "dotenv"
+import UserModel from "../Models/UserModel";
 
 dotenv.config();
 
